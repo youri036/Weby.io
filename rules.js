1b5c46ffb746513eaf38d6410ff1fd9ba1971abc
youri036 <139352645+youri036@users.noreply.github.com>
2024-01-08 13:54:34 +0100

    Add files via upload

---
+++ rules.js
@@ -0,0 +1,20 @@
+function changePageA() {
+    // You can replace 'newpage.html' with the desired URL
+    window.location.href = 'app.html';
+}
+function changePageC() {
+    // You can replace 'newpage.html' with the desired URL
+    window.location.href = 'cource.html';
+}
+function changePageD() {
+    // You can replace 'newpage.html' with the desired URL
+    window.location.href = 'doc.html';
+}
+function changePageH() {
+    // You can replace 'newpage.html' with the desired URL
+    window.location.href = 'index.html';
+}
+function changePageP() {
+    // You can replace 'newpage.html' with the desired URL
+    window.location.href = 'prog.html';
+}
